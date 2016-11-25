0a161e8c436e76ba362495d578d381d2003e9b03
Ziyao Ye <ziyao.ye@mail.utoronto.ca>
2016-11-25 00:33:18 -0500

    add profile routes

--- controller/profile.js
+++ controller/profile.js
@@ -1,3 +1,53 @@
 /**
  * Created by YuAng on 2016-11-22.
  */
+
+//------------------Ziyao 2016-11-24----------------------------
+
+var Artists = require('../model/artists.js');
+
+exports.getById = function(req, res) {
+
+	var targetid = req.query.id;
+
+	Artists.findOne({ id: targetid }, function(err, artist) {
+ 		if (err) throw err;
+
+  		res.send(artist);
+	});
+};
+
+exports.addApplicants = function(req, res) {
+
+	var id = req.query.id;
+	var newArtist = new Artists({
+
+	});
+
+	newArtist.save(function(err){
+		if (err) throw err;
+	});
+};
+
+exports.getArtistBylastname = function(req, res) {
+	var targetlastname = req.query.lname;
+
+	Artists.findOne({ lastname: targetlastname }, function(err, artist) {
+ 		if (err) throw err;
+
+  		res.send(artist);
+	});
+
+};
+
+exports.getArtistByCountry = function(req, res) {
+	var targetcountry = req.query.country;
+
+	Artists.find({ country: targetcountry }, function(err, artist) {
+ 		if (err) throw err;
+
+  		res.send(artist);
+	});	
+}
+
+//-----------------2016-11-24-------------------------------------------
