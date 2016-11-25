20f7b1c89393ba12f3ec5e4261996cad4d12e0e8
zili xie <zili.xie@mail.utoronto.ca>
2016-11-25 16:37:45 -0500

    add product

--- controller/products.js
+++ controller/products.js
@@ -1,8 +1,7 @@
 /**
  * Created by YuAng on 2016-11-22.
  */
-var fs = require('fs');
-var Product = require('../models/product');
+var Products = require('../model/products.js');
 
 //sort by product name
 function pnamesort(a, b) {
@@ -28,52 +27,55 @@ exports.addProduct = function(req, res) {
 };
 
 
-
 //delete one product
 exports.removeProductbyName = function(req, res) {
-    var productname = req.query.name;
-    var a = Product['products'].length;
-    for(i = 0; i < productObj['products'].length; i++) {
-        if (Product['products'][i].name.toUpperCase() == productname.toUpperCase()) {
-            Product['products'].splice(i,1);
+    if(!req.params.id){
+        sendJsonRes(res, 404, {
+            "message": "Found no id in request URL"
+        });
+        return;
+    }
+    Products
+        .findById(req.params.id)
+        .exec(function(err, product){
+        if(!product){
+            sendJsonRes(res, 404, {
+                "message": "Found no match"
+            })
         }
-    }
-    var b = Product['products'].length;
-    if (a == b) {
-        res.send("Error: no such product");
-    }
-    else {
-        res.send("Success");
-    }
+        else if(err){
+            sendJsonRes(res, 404, err);
+        }
+        Products.remove(function(err, product){
+        if(err){
+            console.log(err);
+            return;
+        }
+        sendJsonRes(res, 204, null);
+        })
+}
+		
+
+
+//get product by name (global search)
+exports.getProductbyName = function(req, res) {
+    var productname = req.query.name;
+    Products.findOne({ name: productname }, function(err, product) {
+	if (err) throw err;
+
+	res.send(product);
+    });
+
 };
 
+//get product by release time (global search)
+exports.getProductbyReleaseTime = function(req, res) {
+    var productReleaseTime = req.query.rtime;
+    Products.findOne({ releaseTime: productReleaseTime }, function(err, product) {
+	if (err) throw err;
 
-//get product by name
-exports.getProduct = function(req, res) {
-    var temp;
-    var productname = req.query.name;
-    var pReleaseTime = req.query.releaseTime;
-    var productObj = JSON.parse(JSON.stringify(Product));
-    //productObj['name'].sort(pnamesort);
+	res.send(product);
+    });
 
-    //by family name
-    if (productname != undefined){
-        for(i = 0; i < productObj['products'].length; i++) {
-            if (productObj['products'][i].name.toUpperCase() == productname.toUpperCase()) {
-                temp = productObj['products'][i];
-            }
-        }
-    }
-
-    //by release time
-    if (pReleaseTime != undefined){
-        for(i = 0; i < productObj['products'].length; i++) {
-            if (productObj['products'][i].releaseTime == pReleaseTime) {
-                temp = productObj['products'][i];
-            }
-        }
-    }
-
-    res.send(temp);
 };
 
