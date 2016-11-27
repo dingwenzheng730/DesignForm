aa26aa1b44e588a8692ea3da0384c23120e50480
wenzheng ding <wenzheng.ding@mail.utoronto.ca>
2016-11-26 21:32:32 -0500

    change the links

--- script/user.js
+++ script/user.js
@@ -1,25 +1,5 @@
-// set the global variable to describe the behaviours
-var profile = "<img src='assets/pictures/face.jpg' height='200px'>"
-// set the behaviour when we click on the home button
-function go_home() {
-    document.getElementById("changing_part").innerHTML = "<h1>Welcome!</h1>";
-}
-
-// set the behaviour when we click on the profile button
-function get_profile() {
-    document.getElementById("changing_part").innerHTML = profile;
-}
-
-
-
-
 
 $(document).ready(function() {
-    // set the button for go home
-    $("#go_home_btn").on("click", go_home);
-
-    // set the button for getting profile
-    $("#get_profile_btn").on("click", get_profile);
 
   $("[data-toggle]").click(function() {
     var toggle_el = $(this).data("toggle");
